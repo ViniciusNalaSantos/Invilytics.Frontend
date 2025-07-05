988c5871d925f4b838d3d4f0ac2bfeb77b7ac4db
ViniciusNalaSantos <vinicius.nala@gmail.com>
2025-07-05 14:48:56 -0300

    Created overview and investments page components.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: 'overview',
+        loadComponent: () => import('./features/overview/overview.component').then(m => m.OverviewComponent)
+    },
+    {
+        path: 'investments',
+        loadComponent: () => import('./features/investments/investments.component').then(m => m.InvestmentsComponent)
+    }
+];

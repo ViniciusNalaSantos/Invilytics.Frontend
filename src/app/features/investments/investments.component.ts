ef71dd00cfe99f5f5ed3265fc39887f550588cc7
ViniciusNalaSantos <vinicius.nala@gmail.com>
2025-08-08 11:22:49 -0300

    Development of investments page.

--- src/app/features/investments/investments.component.ts
+++ src/app/features/investments/investments.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { InvestmentsGeneralUpperPanelComponent } from '../../shared/components/investments-general-upper-panel/investments-general-upper-panel.component';
 
 @Component({
   selector: 'app-investments',
-  imports: [],
+  imports: [InvestmentsGeneralUpperPanelComponent],
   templateUrl: './investments.component.html',
   styleUrl: './investments.component.scss'
 })

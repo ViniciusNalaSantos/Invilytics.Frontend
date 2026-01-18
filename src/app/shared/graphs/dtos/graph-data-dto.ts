93fa46185337c68f09df2de8e5c1511489da9cd8
ViniciusNalaSantos <vinicius.nala@gmail.com>
2026-01-18 12:26:27 -0300

    Line graph working.

--- src/app/shared/graphs/dtos/graph-data-dto.ts
+++ src/app/shared/graphs/dtos/graph-data-dto.ts
@@ -1,4 +1,12 @@
 export interface GraphLabelsDto {
     key: string;
     label: string;
+};
+
+export interface LineGraphDataDto {
+    label: string;
+    data: any[];
+    borderColor: string;
+    backgroundColor: string;
+    tension: number;
 }
--- src/app/shared/graphs/line-graph/line-graph.ts
+++ src/app/shared/graphs/line-graph/line-graph.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import {
   Chart,
   LineController,
@@ -8,6 +8,7 @@ import {
   Title,
   CategoryScale
 } from 'chart.js';
+import { GraphLabelsDto, LineGraphDataDto } from '../dtos/graph-data-dto';
 
 Chart.register(
   LineController,
@@ -24,29 +25,81 @@ Chart.register(
   templateUrl: './line-graph.html',
   styleUrl: './line-graph.css'
 })
-export class LineGraphComponent {
+export class LineGraphComponent implements AfterViewInit, OnChanges {
   @Input() title = '';
-  private hasTitle = this.title != '';
+  private hasTitle = false;
   @Input() hasLegend = true;
+  @Input() labels: GraphLabelsDto[] = [
+    { key: 'jan', label: 'Jan' },
+    { key: 'feb', label: 'Feb' },
+    { key: 'may', label: 'May' },
+    { key: 'apr', label: 'Apr' },
+    { key: 'mar', label: 'Mar' }
+  ];
+  @Input() data: LineGraphDataDto[] = [
+    {
+      label: 'Sales',
+      data: [{jan: 120}, {feb: 150}, {may:180}, {apr:170}, {mar:200}],
+      borderColor: 'rgb(75, 192, 192)',
+      backgroundColor: 'rgba(75, 192, 192, 0.2)',
+      tension: 0.4
+    }
+  ];
+  private displayedData: any[] = [];
+  private displayedLabels: string[] = [];
 
-  ngAfterViewInit(): void {
+  ngOnChanges(changes: SimpleChanges): void {
+    this.hasTitle = !!this.title;
+  }
+
+  ngAfterViewInit() {
+    this.hasTitle = !!this.title;
+    this.buildChartData();
+    this.buildLabels();
+    console.log(this.displayedData);
+    console.log(this.displayedLabels);
+
     this.createChart();
+  }
+
+  private buildChartData(): void {
+    this.data.forEach(( d ) => {
+      let dataList = this.orderDataAccordingToLabelSequence(d.data);
+      let displayableData = {
+        label: d.label,
+        data: dataList,
+        borderColor: d.borderColor,
+        backgroundColor: d.backgroundColor,
+        tension: d.tension
+      }
+      this.displayedData.push(displayableData);
+    });
+  }
+
+  private orderDataAccordingToLabelSequence(singleDataGraphDto: Record<string, number>[]): number[] {
+    const result: number[] = [];
+
+    this.labels.forEach(({ key, label }) => {
+      const match = singleDataGraphDto.find(d => d[key] !== undefined);
+      
+      if (match) {
+        result.push(match[key]);
+      }
+    });
+
+    return result;
+  }
+
+  private buildLabels(): void {
+    this.displayedLabels = this.labels.map(l => l.label);
   }
 
   createChart() {
     new Chart('lineChart', {
       type: 'line',
       data: {
-        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
-        datasets: [
-          {
-            label: 'Sales',
-            data: [120, 150, 180, 170, 200],
-            borderColor: 'rgb(75, 192, 192)',
-            backgroundColor: 'rgba(75, 192, 192, 0.2)',
-            tension: 0.4
-          }
-        ]
+        labels: this.displayedLabels,
+        datasets: this.displayedData
       },
       options: {
         responsive: true,
